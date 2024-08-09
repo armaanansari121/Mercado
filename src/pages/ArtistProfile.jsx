84efaa02e74948f2238f704e88deb56f258585f5
armaanansari121 <armaanm2a@gmail.com>
2024-08-09 16:26:54 +0530

    MainMarket Bug fix

--- src/pages/ArtistProfile.jsx
+++ src/pages/ArtistProfile.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import { cn } from "../utils/cn";
 import { useStateContext } from "../contexts";
@@ -11,8 +10,6 @@ import { Input } from "../ui/input";
 const ArtistProfile = () => {
   const { ArtistsContract, ERC1155_CONTRACT, account } = useStateContext();
   const [metadata, setMetadata] = useState([]);
-  // console.log(metadata);
-  // const [profileImage, setProfileImage] = useState(null);
   const address0 = "0x0000000000000000000000000000000000000000";
   const [artist, setArtist] = useState({
     name: "",
@@ -26,7 +23,7 @@ const ArtistProfile = () => {
     description: "",
     price: "",
   });
-  // console.log(account);
+
   useEffect(() => {
     const fetchArtistInfo = async () => {
       try {
@@ -89,17 +86,7 @@ const ArtistProfile = () => {
       fetchMarketData();
     }
   }, [ERC1155_CONTRACT, artist.nftMarkets]);
-  // const handleImageChange = (event) => {
-  //   const file = event.target.files[0];
-  //   if (file) {
-  //     const reader = new FileReader();
-  //     reader.onload = () => {
-  //       setProfileImage(reader.result);
-  //     };
-  //     reader.readAsDataURL(file);
-  //   }
-  // };
-  // console.log(ArtistsContract);
+
   const handleRegisterArtist = async (e) => {
     e.preventDefault();
     try {
@@ -132,29 +119,7 @@ const ArtistProfile = () => {
         "relative flex min-h-screen flex-col items-center justify-center overflow-auto bg-slate-950 w-full"
       )}
     >
-      {/* <div className="justify-center items-center mt-[8rem] text-slate-400 text-8xl">
-        ARTIST
-      </div> */}
-
       <div className="flex items-center">
-        {/* <div className="w-[20rem] h-[20rem] border-2 bg-white rounded-full mr-[60px] translate-x-[8rem] translate-y-[1rem]">
-          {profileImage ? (
-            <img
-              src={profileImage}
-              className="w-full h-full rounded-full object-cover"
-              alt="Profile"
-            />
-          ) : (
-            <div className="flex mt-[9rem] ml-[4rem]">
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-              />
-            </div>
-          )}
-        </div> */}
-
         <div className="px-[10rem] py-[2rem] rounded-md mb- mr-0">
           <AnimatedText>
             <div className="list-decimal text-slate-400 px-10 text-center">
@@ -171,11 +136,6 @@ const ArtistProfile = () => {
               {isRegistered ? (
                 <>
                   <div className="rounded-md mb-4 flex flex-col items-center ">
-                    {/* <img
-                src={profileImage || "/placeholder.jpg"}
-                className="h-52 w-52 mb-4 rounded "
-                alt={artist.name}
-              /> */}
                     <div className="text-sm text-center text-slate-400 text-wrap">
                       <p>
                         <strong>Name:</strong> {artist.name}
@@ -193,17 +153,19 @@ const ArtistProfile = () => {
                   </div>
                 </>
               ) : (
-                <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-                  <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+                <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-slate-900 border border-slate-800">
+                  <h2 className="font-bold text-xl text-slate-200">
                     Register as an Artist
                   </h2>
-                  <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+                  <p className="text-slate-400 text-sm max-w-sm mt-2">
                     Fill in the form to register as an artist.
                   </p>
 
                   <form className="my-8" onSubmit={handleRegisterArtist}>
                     <LabelInputContainer className="mb-4">
-                      <Label htmlFor="name">Name</Label>
+                      <Label htmlFor="name" className="text-slate-200">
+                        Name
+                      </Label>
                       <Input
                         id="name"
                         value={artist.name}
@@ -212,10 +174,11 @@ const ArtistProfile = () => {
                         }
                         placeholder="Your Name"
                         type="text"
+                        className="bg-slate-800 border-slate-700 text-slate-200 placeholder-slate-500"
                       />
                     </LabelInputContainer>
                     <button
-                      className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                      className="bg-gradient-to-br relative group/btn from-cyan-900 to-blue-900 block w-full text-slate-200 rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#1e40af40_inset,0px_-1px_0px_0px_#1e40af40_inset]"
                       type="submit"
                     >
                       Register
@@ -227,17 +190,19 @@ const ArtistProfile = () => {
             </AnimatedText>
           </div>
           <AnimatedText>
-            <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-              <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+            <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-slate-900 border border-slate-800">
+              <h2 className="font-bold text-xl text-slate-200">
                 Create Art Request
               </h2>
-              <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+              <p className="text-slate-400 text-sm max-w-sm mt-2">
                 Fill in the form to create an art request.
               </p>
 
               <form className="my-8" onSubmit={handleArtRequestCreate}>
                 <LabelInputContainer className="mb-4">
-                  <Label htmlFor="description">Description</Label>
+                  <Label htmlFor="description" className="text-slate-200">
+                    Description
+                  </Label>
                   <Input
                     id="description"
                     value={newArtRequest.description}
@@ -249,10 +214,13 @@ const ArtistProfile = () => {
                     }
                     placeholder="Request Description"
                     type="text"
+                    className="bg-slate-800 border-slate-700 text-slate-200 placeholder-slate-500"
                   />
                 </LabelInputContainer>
                 <LabelInputContainer className="mb-4">
-                  <Label htmlFor="price">Price (MER)</Label>
+                  <Label htmlFor="price" className="text-slate-200">
+                    Price (MER)
+                  </Label>
                   <Input
                     id="price"
                     value={newArtRequest.price}
@@ -264,10 +232,11 @@ const ArtistProfile = () => {
                     }
                     placeholder="Request Price"
                     type="number"
+                    className="bg-slate-800 border-slate-700 text-slate-200 placeholder-slate-500"
                   />
                 </LabelInputContainer>
                 <button
-                  className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                  className="bg-gradient-to-br relative group/btn from-cyan-900 to-blue-900 block w-full text-slate-200 rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#1e40af40_inset,0px_-1px_0px_0px_#1e40af40_inset]"
                   type="submit"
                 >
                   Create Request
@@ -281,12 +250,12 @@ const ArtistProfile = () => {
           <div className="flex flex-col items-center mt-4 w-[50rem] h-[35rem] overflow-y-scroll justify-evenly space-y-6 pt-8">
             {isRegistered ? (
               <>
-                <h1 className="text-4xl ">OWNED NFTs</h1>
+                <h1 className="text-4xl text-slate-200">OWNED NFTs</h1>
                 <div className="flex items-center flex-wrap justify-around">
                   {metadata.map((item, index) => (
                     <div
                       key={index}
-                      className="rounded-md border-2 w-72 h-80 mb-4 flex flex-col items-center"
+                      className="rounded-md mx-10 border-2 border-slate-700 w-72 h-80 mb-4 flex flex-col items-center bg-slate-900"
                     >
                       <img
                         src={item?.image}
@@ -315,58 +284,13 @@ const ArtistProfile = () => {
                 </div>
               </>
             ) : (
-              <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input dark:bg-black">
+              <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-slate-900 border border-slate-800 text-slate-200">
                 PLEASE REGISTER
               </div>
             )}
           </div>
         </AnimatedText>
       </div>
-      {/* <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-          Create Art Request
-        </h2>
-        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Fill in the form to create an art request.
-        </p>
-
-        <form className="my-8" onSubmit={handleArtRequestCreate}>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="description">Description</Label>
-            <Input
-              id="description"
-              value={newArtRequest.description}
-              onChange={(e) =>
-                setNewArtRequest({
-                  ...newArtRequest,
-                  description: e.target.value,
-                })
-              }
-              placeholder="Request Description"
-              type="text"
-            />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="price">Price (MER)</Label>
-            <Input
-              id="price"
-              value={newArtRequest.price}
-              onChange={(e) =>
-                setNewArtRequest({ ...newArtRequest, price: e.target.value })
-              }
-              placeholder="Request Price"
-              type="number"
-            />
-          </LabelInputContainer>
-          <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Create Request
-            <BottomGradient />
-          </button>
-        </form>
-      </div> */}
     </div>
   );
 };
@@ -377,7 +301,7 @@ const BottomGradient = () => {
   return (
     <>
       <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
     </>
   );
 };
